5a501f29543c06a4df603bbe4f3b0568e04ed8af
aletafoya <ale.tufk@gmail.com>
2025-09-05 22:59:13 -0600

    api_key

--- GPSalarm/src/app/App.tsx
+++ GPSalarm/src/app/App.tsx
@@ -1,12 +1,10 @@
-import "./global.css"
+import "./global.css";
 import { Text, View } from "react-native";
- 
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-3xl font-bold text-black">Hello, World!</Text>
     </View>
   );
 }
