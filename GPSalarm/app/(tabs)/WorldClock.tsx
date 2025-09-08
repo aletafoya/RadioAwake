7842fbd5dece15076bcc523f99399921f7e331ec
zahid <zahidduron@gmail.com>
2025-09-07 19:01:18 -0600

    add tabs navigation

---
+++ GPSalarm/app/(tabs)/WorldClock.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+ 
+export default function WorldClock() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        WorldClock
+      </Text>
+    </View>
+  );
+}
