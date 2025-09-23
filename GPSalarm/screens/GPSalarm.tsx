de5e202070b30f8993a3637c77c90d7970416ef2
aletafoya <ale.tufk@gmail.com>
2025-09-22 23:52:28 -0600

    showing map and installing dependencies

--- GPSalarm/screens/GPSalarm.tsx
+++ GPSalarm/screens/GPSalarm.tsx
@@ -1,7 +1,23 @@
-import { View, Text } from "react-native";
-import RingtoneList from "../components/RingtoneList";
+import { StyleSheet, View, Text } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
 export default function GPSalarm() {
     return (
-        <RingtoneList></RingtoneList>        
-    )
+        <View className="w-full h-1/2">
+            <MapView 
+                provider={PROVIDER_GOOGLE} 
+                style={styles.map}
+            />
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%', 
+        height: '50%',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
