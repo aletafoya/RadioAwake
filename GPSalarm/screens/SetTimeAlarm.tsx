054034e32f36885a0d6e9aca916e2980a9e27d3d
zahid <zahidduron@gmail.com>
2025-10-07 15:16:43 -0600

    install calendar component

--- GPSalarm/screens/SetTimeAlarm.tsx
+++ GPSalarm/screens/SetTimeAlarm.tsx
@@ -1,11 +1,33 @@
 import { Text, View } from "react-native";
 import CustomHeader from "../components/CustomHeader";
+import ScrollTime from "../components/ScrollTime";
+import { useState } from "react";
+import { Calendar, LocaleConfig } from 'react-native-calendars'
 
 export default function SetTimeAlarm() {
+    const hours = Array.from({ length: 12 }, (_, i) => i.toString().padStart(2, "0"));
+    hours.push("");
+    const minutes = Array.from({ length: 60 }, (_, i) => i.toString().padStart(2, "0"));
+    minutes.push("");
+    const [hrs, setHours] = useState("00")
+    const [mins, setMinutes] = useState("00")
+
     return (
         <View className="flex-1 bg-background">
-            <CustomHeader title={"Set alarm"} back={true}/>
-            <Text className="text-mainText">SetTimeAlarm</Text>
+            <CustomHeader title={"Set alarm"} back={true} />
+            <View className="flex-row justify-center items-center gap-10 mt-10">
+                <ScrollTime data={hours} name={"Hours"} onChange={(val) => setHours(val)} />
+                <Text className="text-mainText text-8xl">:</Text>
+                <ScrollTime data={minutes} name={"Minutes"} onChange={(val) => setMinutes(val)} />
+                <View className="gap-3">
+                    <Text className="text-mainText text-3xl font-semibold">AM</Text>
+                    <Text className="text-mainText text-3xl font-semibold">PM</Text>
+                </View>
+            </View>
+            <View>
+                <Calendar/>
+            </View>
+
         </View>
     )
 }
